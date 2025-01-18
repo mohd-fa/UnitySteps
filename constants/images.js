c5cd49a038aa1c129186cd25187c362b7c1cc79c
Mohammed FA <mohammedfazalabdulla@gmail.com>
2025-01-18 14:46:50 +0530

    edit

--- constants/images.js
+++ constants/images.js
@@ -1,6 +1,6 @@
-import home from '../assets/images/home.png';
-import profile from '../assets/images/profile.png';
-import task from '../assets/images/task.png';
+import home from '../assets/images/home.svg';
+import profile from '../assets/images/profile.svg';
+import task from '../assets/images/task.svg';
 import expert from '../assets/images/expert.svg';
 
 export default { home, profile, task, expert };
