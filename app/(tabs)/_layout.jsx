61122e7ae67c90d27ec060c9d02dc16016120c8a
Mohammed FA <mohammedfazalabdulla@gmail.com>
2025-01-18 13:21:12 +0530

    ts to js

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { images } from "../../constants";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
@@ -11,6 +31,14 @@ const TabsLayout = () => {
           options={{
             title: "Home",
             headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={images.home}
+                color={color}
+                name="Account"
+                focused={focused}
+              />
+            ),
           }}
         ></Tabs.Screen>
         <Tabs.Screen
---
+++ constants/images.js
@@ -0,0 +1,6 @@
+import home from '../assets/images/home.png';
+import profile from '../assets/images/profile.png';
+import task from '../assets/images/task.png';
+import expert from '../assets/images/expert.svg';
+
+export default { home, profile, task, expert };
